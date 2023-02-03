4cb2ed9c2da4945da8ac483f267b76e66b54edb6
vince <vincedavince@163.com>
2023-02-03 16:30:19 +0800

    原地妥协！

---
+++ src/main/java/com/ohyoung/algorithmprictise/AlgorithmPrictiseApplication.java
@@ -0,0 +1,13 @@
+package com.ohyoung.algorithmprictise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmPrictiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmPrictiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohyoung/algorithmprictise/AlgorithmPrictiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohyoung.algorithmprictise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmPrictiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
